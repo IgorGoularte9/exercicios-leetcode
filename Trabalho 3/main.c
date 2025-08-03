48beadc4c3d799c46b5123ac0faf245c64d8bd6f
Igor Pereira Goularte <117081628+IgorGoularte9@users.noreply.github.com>
2025-08-03 17:55:44 -0300

    Update main.c

--- Trabalho 3/main.c
+++ Trabalho 3/main.c
@@ -1,1 +1,28 @@
+ struct TreeNode* balanceBST(struct TreeNode* raiz) {
+    struct TreeNode* nos[1000]; 
+    int indice = 0;
+    percursoEmOrdem(raiz, nos, &indice);
+    return construirArvoreBalanceada(nos, 0, indice - 1);
+}
+
+void percursoEmOrdem(struct TreeNode* raiz, struct TreeNode** nos, int* indice) {
+    if (raiz == NULL) {
+        return;
+    }
+    percursoEmOrdem(raiz->left, nos, indice);
+    nos[(*indice)++] = raiz; 
+    percursoEmOrdem(raiz->right, nos, indice);
+}
+
+struct TreeNode* construirArvoreBalanceada(struct TreeNode** nos, int ini, int fim) {
+    if (ini > fim) {
+        return NULL;
+    }
+    int meio = ini + (fim - ini) / 2;
+    struct TreeNode* raiz = nos[meio];
+    raiz->left = construirArvoreBalanceada(nos, ini, meio - 1); 
+    raiz->right = construirArvoreBalanceada(nos, meio + 1, fim); 
+    return raiz;
+}
+
 
